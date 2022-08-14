3b4376e7cd56f7af443c7185b46b840acc6eddf3
Victor Vavulin <vvavulin@gmail.com>
2022-08-14 20:23:08 +0300

    removed the "size" property

--- src/main/java/DynamicArray.java
+++ src/main/java/DynamicArray.java
@@ -4,27 +4,24 @@ import java.util.Arrays;
 
 public class DynamicArray {
 
-    private int size;
     private Object[] list;
 
     public DynamicArray() {
         this.list = new Object[]{};
-        this.size = 0;
     }
 
     public int getSize() {
-        return this.size;
+        return this.list.length;
     }
 
     public void add(Object item) {
-        this.size ++;
-        this.list = Arrays.copyOf(this.list, this.size);
-        this.list[this.size - 1] = item;
+        this.list = Arrays.copyOf(this.list, this.getSize()+1);
+        this.list[this.getSize() - 1] = item;
 
     }
 
     public Object getItem(int index) {
-        if (index >= 0 &&  index < this.size) {
+        if (index >= 0 &&  index < this.getSize()) {
             return this.list[index];
         }
         else {
