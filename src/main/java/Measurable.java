437a64d36f36bab7e7f9933253220cce4e737c85
victor <vvavulin@gmail.com>
2022-09-09 22:29:02 +0200

    MesaurableList based on array

--- src/main/java/Measurable.java
+++ src/main/java/Measurable.java
@@ -11,6 +11,17 @@ public class Measurable {
         this.width = width;
     }
 
+    public boolean equals(Measurable m) {
+        if (this.length == m.length && this.width == m.width) {
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Length: " + this.length + ", width: " + this.width;
+    }
+
     public int calcArea() {
         return Math.multiplyExact(this.length, this.width);
     }
